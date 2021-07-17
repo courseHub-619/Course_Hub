bc09f60e230fd5ee010815de8551e1570b46ec46
Lotfi-Ch <lotfichakchouk1@gmail.com>
2021-07-17 16:40:01 +0100

    first push

---
+++ coursehub/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    purge: ['./pages/**/*.js', './styles/**/*.css'],
+    
+    future:{
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+}
