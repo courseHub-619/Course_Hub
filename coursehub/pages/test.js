e67ae073e4a5dc884a06d01b800f9de8d6043a5c
houssembouraioui <houssembouraoui6@gmail.com>
2021-07-21 00:51:13 +0100

    public teacher profile

--- coursehub/pages/test.js
+++ coursehub/pages/test.js
@@ -1,57 +1,32 @@
-// import { PrismaClient } from '@prisma/client'
-import { useCallback, useEffect, useState } from 'react'
-// const prisma = new PrismaClient()
-
-// export const getStaticProps = async () => {
-//     const students = await prisma.student.findMany()
-//     console.log("student", students)
-
-//     return {
-//         props: {
-//             data: students,
-//         }
-//     }
-// }
-
-import axios from "axios"
+import axios from "axios";
 
 function test(props) {
-    console.log(props)
-    return (
-        // <div>test</div>
+  console.log(props);
+  return (
 
-        <div>
-
-
-            {props.data.map((std) => (
-                <div key={std.student_id}>
-                    <div> {std.userName} </div>
-                    <div> {std.education} </div>
-                    <div> {std.age} </div>
-
-                </div>
-            ))}
-
+    <div>
+      {props.data.map((std) => (
+        <div key={std.student_id}>
+          <div> {std.userName} </div>
+          <div> {std.education} </div>
+          <div> {std.age} </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-    try {
-        const res = await axios.get("http://localhost:4200/test/user");
-        const data = res.data
-        return {
-            props: { data }
-        }
+  try {
+    const res = await axios.get("http://localhost:4200/test/user");
+    const data = res.data;
+    return {
+      props: { data },
+    };
+  } catch (error) {
+    console.log("here");
+    return { error };
+  }
+}
 
-    } catch (error) {
-        console.log("here")
-        return { error };
-    }
-};
-
-export default test
-
-
-
-
+export default test;
--- coursehub/server/server.js
+++ coursehub/server/server.js
@@ -1,34 +1,36 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
 
-const PORT = process.env.PORT || 4200
-const dev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || 4200;
+const dev = process.env.NODE_ENV !== "production";
 
-
-
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-
-
-
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const server = express();
-server.use(express.json())
-server.use(express.urlencoded({ extended: false }))
-
-
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 server.get("/test/user", async (req, res) => {
-    console.log("hajaaaaaaaaaaaaa")
-    const students = await prisma.student.findMany()
-    return res.status(201).send(students);
-})
+  console.log("hajaaaaaaaaaaaaa");
+  const students = await prisma.student.findMany();
+  return res.status(201).send(students);
+});
 
+//teacher profile
 
+server.get("user/0", async (req, res) => {
+  console.log("hereeeeeeeeeeeeeee");
+  const teacher = await prisma.teacher.findUnique({
+    where: {
+      teacher_id: 0,
+    },
+  });
+  console.log(teacher);
+  return res.status(201).send(teacher);
+});
 
-server.listen(PORT, err => {
-    if (err) throw err;
-    console.log(`Example app listening at http://localhost:${PORT}`)
-})
-
-
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
