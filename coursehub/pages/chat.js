06ea36386db1620296801aaa2ea6d29095719e27
Lotfi-Ch <lotfichakchouk1@gmail.com>
2021-07-28 14:47:52 +0200

    last

--- coursehub/pages/chat.js
+++ coursehub/pages/chat.js
@@ -1,39 +1,35 @@
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import React, { useEffect, useRef, useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import TextField from "@material-ui/core/TextField"
+import AssignmentIcon from "@material-ui/icons/Assignment"
+import PhoneIcon from "@material-ui/icons/Phone"
+import React, { useEffect, useRef, useState } from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
 import Peer from "simple-peer"
 import io from "socket.io-client"
-import { FcVideoCall, FcEndCall } from "react-icons/fc";
-import { FiPhoneCall } from "react-icons/fi";
-
-const socket = io.connect("http://localhost:4200")
 
 
-const Chat = () => {
+
+const socket = io.connect('http://localhost:4200')
+function App() {
     const [me, setMe] = useState("")
-    const [stream, setStream] = useState("")
+    const [stream, setStream] = useState()
     const [receivingCall, setReceivingCall] = useState(false)
     const [caller, setCaller] = useState("")
     const [callerSignal, setCallerSignal] = useState()
     const [callAccepted, setCallAccepted] = useState(false)
-    const [idToCall, setidToCall] = useState("")
+    const [idToCall, setIdToCall] = useState("")
     const [callEnded, setCallEnded] = useState(false)
     const [name, setName] = useState("")
-
     const myVideo = useRef()
     const userVideo = useRef()
     const connectionRef = useRef()
 
-
-
     useEffect(() => {
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-            .then((stream) => {
-                setStream(stream)
-                myVideo.current.srcObject = stream
-            })
-
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+            setStream(stream)
+            myVideo.current.srcObject = stream
+        })
 
         socket.on("me", (id) => {
             setMe(id)
@@ -45,7 +41,6 @@ const Chat = () => {
             setName(data.name)
             setCallerSignal(data.signal)
         })
-
     }, [])
 
     const callUser = (id) => {
@@ -54,10 +49,7 @@ const Chat = () => {
             trickle: false,
             stream: stream
         })
-
-
         peer.on("signal", (data) => {
-
             socket.emit("callUser", {
                 userToCall: id,
                 signalData: data,
@@ -65,18 +57,17 @@ const Chat = () => {
                 name: name
             })
         })
-
         peer.on("stream", (stream) => {
-            userVideo.current.srcObject = stream
-        })
 
+            userVideo.current.srcObject = stream
+
+        })
         socket.on("callAccepted", (signal) => {
             setCallAccepted(true)
             peer.signal(signal)
         })
 
         connectionRef.current = peer
-
     }
 
     const answerCall = () => {
@@ -86,11 +77,9 @@ const Chat = () => {
             trickle: false,
             stream: stream
         })
-
         peer.on("signal", (data) => {
             socket.emit("answerCall", { signal: data, to: caller })
         })
-
         peer.on("stream", (stream) => {
             userVideo.current.srcObject = stream
         })
@@ -102,74 +91,70 @@ const Chat = () => {
     const leaveCall = () => {
         setCallEnded(true)
         connectionRef.current.destroy()
-
     }
+
     return (
         <>
-
-
-            <div className="p-4 text-center">
-                <div className="flex justify-between p-2">
-                    <div >
-                        {stream && <video playsInline muted ref={myVideo} autoPlay className="w-50" />}
+            <h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
+            <div className="container">
+                <div className="video-container">
+                    <div className="video">
+                        {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
                     </div>
-                    <div >
+                    <div className="video">
                         {callAccepted && !callEnded ?
-                            <video playsInline ref={userVideo} autoPlay className="w-50" /> :
+                            <video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> :
                             null}
                     </div>
                 </div>
-                <div className="p-2 text-center gap-2 flex justify-center">
+                <div className="myId">
                     <TextField
-                        className="p-2"
-                        label="Enter your name"
+                        id="filled-basic"
+                        label="Name"
                         variant="filled"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
+                        style={{ marginBottom: "20px" }}
                     />
-                    <div className="p-2">
-                        <CopyToClipboard text={me} className="p-4">
-                            <Button variant="contained" className="p-4 " >
-                                Copy your ID
-                            </Button>
-                        </CopyToClipboard>
-                    </div>
+                    <CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+                        <Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
+                            Copy ID
+                        </Button>
+                    </CopyToClipboard>
 
                     <TextField
-                        className="p-2"
-                        label="Paste ID to call"
+                        id="filled-basic"
+                        label="ID to call"
                         variant="filled"
                         value={idToCall}
-                        onChange={(e) => setidToCall(e.target.value)}
+                        onChange={(e) => setIdToCall(e.target.value)}
                     />
-
+                    <div className="call-button">
+                        {callAccepted && !callEnded ? (
+                            <Button variant="contained" color="secondary" onClick={leaveCall}>
+                                End Call
+                            </Button>
+                        ) : (
+                            <IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
+                                <PhoneIcon fontSize="large" />
+                            </IconButton>
+                        )}
+                        {idToCall}
+                    </div>
                 </div>
-                <div className="text-center justify-self-center">
-                    {callAccepted && !callEnded ? (
-                        <div style={{ display: "flex", justifyContent: "center", color: "#32CD32" }}>
-                            <FcEndCall size={30} onClick={leaveCall}></FcEndCall>
-                        </div>
-                    ) : (
-                        <div style={{ display: "flex", justifyContent: "center", color: "#32CD32" }}>
-                            <FiPhoneCall size={40} onClick={() => callUser(idToCall)}></FiPhoneCall>
-                        </div>
-                    )}
-                </div>
-                <div className="text-center">
+                <div>
                     {receivingCall && !callAccepted ? (
-                        <div >
+                        <div className="caller">
                             <h1 >{name} is calling...</h1>
-                            <div style={{ display: "flex", justifyContent: "center" }}>
-                                <FcVideoCall size={50} onClick={answerCall}>
-                                    Answer
-                                </FcVideoCall>
-                            </div>
+                            <Button variant="contained" color="primary" onClick={answerCall}>
+                                Answer
+                            </Button>
                         </div>
                     ) : null}
                 </div>
             </div>
         </>
-    );
+    )
 }
 
-export default Chat;
+export default App;
